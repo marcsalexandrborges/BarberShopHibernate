3c434584ca8459ffcdfbeb68a11a467e23b9eb52
Marcos Barbosa <marcsalexandr@gmail.com>
2020-09-03 09:20:21 -0300

    Projeto de uma barbearia usando java e hibernate

---
+++ target/generated-sources/annotations/modelo/Usuario_.java
@@ -0,0 +1,16 @@
+package modelo;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-08-30T00:12:29")
+@StaticMetamodel(Usuario.class)
+public class Usuario_ { 
+
+    public static volatile SingularAttribute<Usuario, String> senha;
+    public static volatile SingularAttribute<Usuario, String> usuario;
+    public static volatile SingularAttribute<Usuario, Integer> id;
+    public static volatile SingularAttribute<Usuario, String> email;
+
+}
